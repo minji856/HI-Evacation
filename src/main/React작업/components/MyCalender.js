808905925f2d75b1332994e7e20cb9341bc4f7e6
minji856 <sistar96@gmail.com>
2023-12-08 17:44:11 +0900

    axios로 서버에 데이터 보내는 코드

--- src/main/React작업/components/MyCalender.js
+++ src/main/React작업/components/MyCalender.js
@@ -3,6 +3,7 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import Axios from "axios";
 import './Mycalendar.css'
 
 const MyCalendar = ()=> {
@@ -13,7 +14,19 @@ const MyCalendar = ()=> {
   const eventClick = (info) => {
     const eventTitle = info.event.title;
     alert(`Clicked on event: ${eventTitle}`);
+
+    sendEventToServer(eventTitle);
   };
+
+  const sendEventToServer = (eventTitle) => {
+    Axios.post('/api/saveEventTitle', { eventTitle })
+      .then(response => {
+        console.log('Server response:', response.data);
+      })
+      .catch(error => {
+        console.error('값이 넘어오지 않았습니다.', error);
+      });
+  }
 
   return( 
         <div className="App">
