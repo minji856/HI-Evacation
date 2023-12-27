d2ceb7ec912a70e4e34a9978baa2c2fe840d3ec3
minji856 <sistar96@gmail.com>
2023-12-27 16:28:52 +0900

    javaDoc 주석화

--- src/main/React작업/components/MyCalender.js
+++ src/main/React작업/components/MyCalender.js
@@ -10,78 +10,86 @@ import './Mycalendar.css'
  * FullCalendar API 사용
  * npm install --save @fullcalendar/react @fullcalendar/core @fullcalendar/daygrid @fullcalendar/interaction @fullcalendar/timegrid
  * npm install style-loader css-loader sass-loader node-sass --save
- * @param dateClick 날짜를 눌러서 일정등록 (현재는 날짜누르면 알림창만 구현함)
- * @param eventClick 일정 상세보기
- * @param events 일정
- * @returns 현재 날짜 지정 이벤트
  */
 
-// 일정을 커스텀 하는 옵션
+/**
+ * 일정 출력 모양을 커스텀 하는 함수
+ * @param eventInfo 이벤트 정보
+ * @return 일정 제목과 시간
+ *  */ 
 const renderEventContent = (eventInfo) => {
   return (
     <>
-      <b>테스트 : </b>
+      <b>{eventInfo.timeText}</b>
       <i>{eventInfo.event.title}</i>
     </>
   )
 }
 
+/**
+ * 날짜를 누르면 입력 프롬프트 생성 후 정보 기입하는 메서드
+ * @param selectInfo 선택된 정보
+ * @return 일정 제목, 시작일, 종료일
+ */
 const handleDateSelect = (selectInfo) => {
   let title = prompt('일정 제목을 입력해주세요.')
   let calendarApi = selectInfo.view.calendar
 
-  calendarApi.unselect() // clear date selection
+  // calendarApi.unselect()
 
+  // 현재 제목만 입력됨
   if (title) {
     calendarApi.addEvent({
-      // id: createEventId(), 아직 메소드 정의 안됨
-      title,
+      title: title,
       start: selectInfo.startStr,
       end: selectInfo.endStr,
-      allDay: selectInfo.allDay
+      // allDay: selectInfo.allDay 시간기능 구현해야지만 됨
     })
   }
 }
 
-// const handleEventClick = (clickInfo) => {
-//   if (window.confirm(`일정을 삭제하시겠습니까?'${clickInfo.event.title}'`)) {
-//     clickInfo.event.remove()
-//   }
-// }
+/**
+ * 일정을 클릭하면 일정이 삭제됨 ( db 연동 해야함 )
+ * 일정 상세보기 구현하기
+ * @param clickInfo 일정 클릭했을 때의 정보
+ */
+const handleEventClick = (clickInfo) => {
+  if (window.confirm(`${clickInfo.event.title} 일정을 삭제하시겠습니까?`)) {
+    clickInfo.event.remove()
+  }
+}
 
 const MyCalendar = ()=> {
   const [events, setEvents] = useState([]);
-  const [currentEvents, setCurrentEvents] = useState([]);
-
-  // const handleEvents = (events) => {
-  //   setCurrentEvents(events);
-  // }
 
   return( 
         <div className="App">
           <FullCalendar 
             plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-            initialView={'dayGridMonth'}
+            initialView={'dayGridMonth'} // 초기 로드 될때 보이는 캘린더 화면(기본 설정: 달)
+            // 해더에 표시할 툴바
             headerToolbar={{
               start: 'today', 
               center: 'title',
               end: 'prev,next' 
             }}
             events={[
-              { title: '창립기념일', date: '2023-12-10' },
+              { title: '창립기념일', // 더미데이터
+                start: '2023-12-10',
+                end: '2023-12-15' },
               { title: '워크샵', date: '2023-12-24' }
             ]}
             editable={true}
-            selectable={true}
-            dayMaxEvents={true}
+            selectable={true} // 달력 일자 드래그 설정가능
+            dayMaxEvents={true} // 일정이 오버되면 높이 제한 +더보기 나오는 기능
             select={handleDateSelect}
             height={"85vh"}
-            eventAdd={function(){}}
+            locale={'ko'} // 한국어 설정
+            eventAdd={function(){}} // 이벤트가 추가되면 발생하는 이벤트
+            eventChange={function(){}} // 이벤트가 수정되면 발생하는 이벤트
             eventContent={renderEventContent}
-            // eventClick={handleEventClick}
-            // eventsSet={handleEvents()}
+            eventClick={handleEventClick}
             // events={events}
-            // events: '/api/demo-feeds/events.json'
           />
         </div>
     )
