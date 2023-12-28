c79100b3eaa624895ee5d861a85641b4945cb193
minji856 <sistar96@gmail.com>
2023-12-28 17:34:05 +0900

    Sidebar 2개씩 출력되던 문제를 Routes 위치를 옮김으로 해결

--- src/main/React작업/App.js
+++ src/main/React작업/App.js
@@ -7,54 +7,34 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './utils/css/sb-admin-2.css';
 
 const App = () => {
-  // const [event, setEvent] = useState("");
-
-  // useEffect(()=>{
-  //   axios.post("/api/events").then((response)=>{
-  //       console.log(response.data);
-  //       setEvent(response.data)
-  //     }
-  //   );
-  // }, []);
-  
-
   return (
     <BrowserRouter>
-            <Routes>
-                <Route path="/event" element={<Eventmanage />} />
-                <Route path="/vac" element={<Vacation />}/>
-            </Routes>
-            <div id="page-top">
-          <div id="wrapper">
-            
-
-            {/* 여기에 sidebar 컴포넌트를 렌더링하는 코드 추가 */}
-              <Sidebar />
-            
-            {/* 여기에 content-wrapper와 topbar 컴포넌트를 렌더링하는 코드 추가 */}
-            <div id="content-wrapper" class="d-felx flex-column">
-            
+      <div id="page-top">
+        <div id="wrapper">
+          {/* sidebar 컴포넌트를 렌더링하는 코드 추가 */}
+            <Sidebar/>
+          {/* content-wrapper와 topbar 컴포넌트를 렌더링하는 코드 추가 */}
+          <div id="content-wrapper" class="d-felx flex-column">
             {/* User Information 에 이름 출력하게하기 */}
             <div id="content">
               <Topbar />
             </div>
-            
-          <div className="container-fluid">
+            <div className="container-fluid">
             {/* 여기에 현재 접속한 사원이름 출력과 연차현황 출력 */}
-            <div class="d-sm-flex align-items-center justify-content-between mb-4"> 
-              <h1 class="h3 mb-0 text-gray-800">기본 Home</h1> 
-            </div>
-
-                <div>
-                  <Footer />
-                </div>
-
+              <Routes>
+                  <Route path="/event" element={<Eventmanage />} />
+                  <Route path="/vac" element={<Vacation />}/>
+                  {/* <Route path="/att" element={<AttendanceTable />}/> */}
+              </Routes>
+              <div>
+                <Footer />
+              </div>
             </div>
           </div>
+          {/* Wrapper 끝 */}
         </div>
-        </div>
+      </div>
     </BrowserRouter>
-    
   );
 }
 
