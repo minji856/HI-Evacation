e300246b3ccd09813f4c312759369da3308b96ab
minji856 <sistar96@gmail.com>
2023-12-29 12:15:39 +0900

    :lipstick: 달력 card-body에 추가

--- src/main/React작업/pages/CalendarPage.js
+++ src/main/React작업/pages/CalendarPage.js
@@ -1,21 +1,22 @@
 import MyCalendar from "../components/MyCalender";
 import '../utils/css/sb-admin-2.css';
 
-const Eventmanage = () => {
+const CalendarPage = () => {
   return (
     <>
-            {/* 여기에 현재 접속한 사원이름 출력과 연차현황 출력 */}
+      {/* 여기에 현재 접속한 사원이름 출력과 연차현황 출력 */}
       <div class="d-sm-flex align-items-center justify-content-between mb-4"> 
         <h1 class="h3 mb-0 text-gray-800">일정관리</h1> 
       </div>
-    {/* Calendar 컴포넌트 추가   */}
-    <div class="row">
-        <div class="col-xl-8 col-lg-7">
-          <MyCalendar />
+      <div class="col-xl-8 col-lg-5">
+        <div class="card shadow mb-4">
+            <div class="card-body">
+              <MyCalendar />
+            </div>
         </div>
-    </div>
-  </>
+      </div>
+    </>
   );
 }
 
-export default Eventmanage;
+export default CalendarPage;
