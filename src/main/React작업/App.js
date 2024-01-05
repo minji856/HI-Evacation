727c364a4c468e61c7c86735becc55c26778e638
minji856 <sistar96@gmail.com>
2024-01-06 01:02:34 +0900

    :poop: 토큰 값 가져오기

--- src/main/React작업/App.js
+++ src/main/React작업/App.js
@@ -1,28 +1,49 @@
 import Footer from "./components/footer";
 import Sidebar from "./components/sidebar";
 import Topbar from "./components/topbar";
-import Eventmanage from "./pages/Eventmanage";
 import Vacation from "./pages/Vacation";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './utils/css/sb-admin-2.css';
+import CalendarPage from "./pages/CalendarPage";
+import { useEffect, useState } from "react";
+import axios from "axios";
+// import './utils/css/sb-admin-2.css';
 
 const App = () => {
+  const [message, setMessage] = useState("");
+
+  useEffect(()=>{
+    axios.get('http://localhost:8484/api/event')
+    .then(response => setMessage(response.data))
+  })
+
+  /* jwt token 가져오기 */
+  fetch("/info",{
+    method: 'get',
+    headers: {
+        'content-type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.getItem("jwt"),
+        // {jwt.secret}
+    }
+    }).then(res => res.json())
+        .then(json => alert("이름 : " + json.name+", 비밀번호 : " + json.pwd))
+        
   return (
     <BrowserRouter>
       <div id="page-top">
         <div id="wrapper">
-          {/* sidebar 컴포넌트를 렌더링하는 코드 추가 */}
             <Sidebar/>
-          {/* content-wrapper와 topbar 컴포넌트를 렌더링하는 코드 추가 */}
-          <div id="content-wrapper" class="d-felx flex-column">
+            <h1>{message}</h1>
+            <h1>{message}</h1>
+            <h1>{message}</h1>
+            <h1>{message}</h1>
+          <div id="content-wrapper" className="d-felx flex-column">
             {/* User Information 에 이름 출력하게하기 */}
             <div id="content">
               <Topbar />
             </div>
             <div className="container-fluid">
-            {/* 여기에 현재 접속한 사원이름 출력과 연차현황 출력 */}
               <Routes>
-                  <Route path="/event" element={<Eventmanage />} />
+                  <Route path="/event" element={<CalendarPage />} />
                   <Route path="/vac" element={<Vacation />}/>
                   {/* <Route path="/att" element={<AttendanceTable />}/> */}
               </Routes>
